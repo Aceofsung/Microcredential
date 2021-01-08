79e9adfba8450951699388767a3bf8d4a270a8f1
Aceofsung <76955873+Aceofsung@users.noreply.github.com>
2021-01-08 13:21:40 -0500

    .

---
+++ Pythonfiles/slicefam.py
@@ -0,0 +1,3 @@
+fam = ["jack", "rachel", "sam", "brian", "abigail", "sally", "ariana", "patrick", "nick", "amy", "jacob", "elizabeth", "sarah", "hamilton", "jackson", "washington", "kaylie"]
+for i in range(1,11):
+  print(fam[i:11])
